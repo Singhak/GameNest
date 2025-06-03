63d8b92a0881448fc544486a38ff4702a5a5473e
singhak <anilsingh.singh09@gmail.com>
2025-06-03 18:52:11 +0530

    feat: add NotificationType enum for notification management

---
+++ src/common/enums/notification-enum.ts
@@ -0,0 +1,8 @@
+export enum NotificationType {
+    BookingPending = 'booking_pending',
+    BookingConfirmed = 'booking_confirmed',
+    BookingRejected = 'booking_rejected',
+    BookingCancelled = 'booking_cancelled',
+    ReviewReceived = 'review_received',
+    SystemMessage = 'system_message',
+}
