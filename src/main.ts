05e25bb90d363b908e2fab656e61fe288027f83b
singhak <anilsingh.singh09@gmail.com>
2025-06-15 13:48:17 +0530

    feat: enable CORS and set global API prefix in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true, // Or specify your frontend's origin, e.g., 'http://localhost:3000'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
