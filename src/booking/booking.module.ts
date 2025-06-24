2b7dd34f5cac370afdfc06d3929f343182f6a6bf
singhak <anilsingh.singh09@gmail.com>
2025-06-24 11:20:55 +0530

    refactor: remove unused NotificationModule import from BookingModule

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -3,10 +3,8 @@ import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
-import { NotificationModule } from '../notification/notification.module'; // Import NotificationModule
 import { UsersModule } from '../users/users.module'; // Import UsersModule
 import { Booking, BookingSchema } from './booking.schema';
-import { SportService, SportServiceSchema } from 'src/sport-service/sport-service.schema';
 import { SportClubModule } from 'src/sport-club/sport-club.module';
 import { SportServiceModule } from 'src/sport-service/sport-service.module';
 
@@ -15,7 +13,6 @@ import { SportServiceModule } from 'src/sport-service/sport-service.module';
     MongooseModule.forFeature([
       { name: Booking.name, schema: BookingSchema },
     ]),
-    forwardRef(() => NotificationModule), 
     forwardRef(() => UsersModule), 
     forwardRef(() => SportServiceModule), 
     forwardRef(() => SportClubModule),
